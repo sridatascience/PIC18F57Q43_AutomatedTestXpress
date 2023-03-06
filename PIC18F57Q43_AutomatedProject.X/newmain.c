616ff24137811c9f33464b30f6bd8c11abf2ff9f
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-03-06 13:37:39 +0530

    Updated the code with new Timing  registers

--- PIC18F57Q43_AutomatedProject.X/newmain.c
+++ PIC18F57Q43_AutomatedProject.X/newmain.c
@@ -14,11 +14,12 @@ uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
 }
 void main() {
    volatile uint8_t a = 0x05;
-   LATA = a;
+   TMR1H = a;
    volatile uint8_t c = 0x1F;
-   TRISA = c;
+   TMR1L = c;
    volatile uint8_t b =0x08;
-   LATB = b;
+   TMR0H = b;
    volatile uint8_t t = add(a,b,c);
+   TMR0L = t;
    return;
 }
